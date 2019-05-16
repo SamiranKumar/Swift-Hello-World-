b445452c772cda9c961d0b742f25a5470e5b0e52
samirankumar11 <samirankumar11@gmail.com>
2019-05-16 11:45:46 +0600

    data type test

--- HeloWorld.playground/Contents.swift
+++ HeloWorld.playground/Contents.swift
@@ -1,5 +1,42 @@
 import UIKit
 
-var str = "Hello, playground"
-var str1 = "Hello HK HK KK HH HR HR RR HH"
+var str1 = "HK HK KK HH HR HR RR HH"
 
+var str = "Hello, playground"
+var year = 2019
+var value = 2.50
+
+var strValue : String = "Samiaran Kumar"
+
+var valueInt :Int  = 1234
+
+
+// varA is inferred to be of type Int
+var varA = 42
+print(varA)
+
+// varB is inferred to be of type Double
+var varB = 3.14159
+print(varB)
+
+// varC is also inferred to be of type Double
+var varC = 3 + 0.14159
+print(varC)
+
+
+
+
+var varA1 = "Godzilla"
+var varB2 = 1000.00
+
+print("Value of \(varA1) is more than \(varB2) millions")
+
+
+
+var myString : String? = nil
+
+if myString != nil {
+    print(myString ?? "<#default value#>")
+} else {
+    print("myString has nil value")
+}
